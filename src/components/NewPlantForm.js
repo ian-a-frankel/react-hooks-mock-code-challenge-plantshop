4035607224d670435631ed8d16a881ace9c9b61a
Ian Frankel <ian.frankel@gmail.com>
2023-11-01 11:32:26 -0400

    plants with pessimistic retrieval function

--- src/components/NewPlantForm.js
+++ src/components/NewPlantForm.js
@@ -1,13 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 
-function NewPlantForm() {
+function NewPlantForm({retrievePlants}) {
+
+  const [newPlant, setNewPlant] = useState({name: '', image: '', price: ''})
+
+  function changePlant(event) {
+    setNewPlant({...newPlant, [event.target.name]: event.target.value})
+    console.log(newPlant)
+  }
+
+  function postPlant(event) {
+    event.preventDefault()
+    fetch('http://localhost:6001/plants' ,{
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      },
+      body: JSON.stringify(newPlant)
+    })
+    .then(() => {retrievePlants()})
+
+  }
+
   return (
     <div className="new-plant-form">
       <h2>New Plant</h2>
-      <form>
-        <input type="text" name="name" placeholder="Plant name" />
-        <input type="text" name="image" placeholder="Image URL" />
-        <input type="number" name="price" step="0.01" placeholder="Price" />
+      <form onSubmit={postPlant} >
+        <input onChange={changePlant} type="text" name="name" placeholder="Plant name" />
+        <input onChange={changePlant} type="text" name="image" placeholder="Image URL" />
+        <input onChange={changePlant} type="number" name="price" step="0.01" placeholder="Price" />
         <button type="submit">Add Plant</button>
       </form>
     </div>
--- src/components/PlantList.js
+++ src/components/PlantList.js
@@ -1,9 +1,9 @@
 import React from "react";
 import PlantCard from "./PlantCard";
 
-function PlantList() {
+function PlantList({displayPlants}) {
   return (
-    <ul className="cards">{/* render PlantCards components in here */}</ul>
+    <ul className="cards">{displayPlants.map(plant => <PlantCard key={plant.id} plant={plant}/>)}</ul>
   );
 }
 
